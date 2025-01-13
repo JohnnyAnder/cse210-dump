c32c9f76ac74d1902ecdb261bd0e8762f1e69727
JohnnyAnder <145506073+JohnnyAnder@users.noreply.github.com>
2025-01-13 16:08:18 -0700

    prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int x = rnd.Next(1, 101);
+        string answer = "n";
+        
+        while (answer == "n")
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int y = int.Parse(guess);
+
+            if (x == y)
+            {
+                Console.WriteLine("You guessed it!");
+                answer = "y";
+            }
+            else if (x > y)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
